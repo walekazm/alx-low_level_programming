98f2d580afb1c49a6307c68bd80d02317653f451
walekazm <kazeem.nasiru@gmail.com>
2022-09-08 10:17:28 -0700

    ALL TASK

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,8 @@
  * Return: Always 0 (success)
  */
 
-	int main (void)
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
