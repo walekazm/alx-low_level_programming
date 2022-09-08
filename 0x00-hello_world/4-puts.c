804fd9b7c6efd4604896764d79ec2a13cb5291d3
walekazm <kazeem.nasiru@gmail.com>
2022-09-08 09:55:37 -0700

    ALL TASK

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always ) (success)
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n);
+	retun (0);
+}
+
