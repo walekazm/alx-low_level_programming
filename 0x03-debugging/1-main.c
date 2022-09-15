55e37b1bc152f425bc47fda4900b979022f478bf
walekazm <kazeem.nasiru@gmail.com>
2022-09-15 07:17:27 -0700

    all task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,9 +11,11 @@ int main(void)
 {
 	int i;
 
-	/*
-	 * printf("Infinite loop incoming :(\n");
-	 * i = 0;
+	 printf("Infinite loop incoming :(\n");
+
+	 i = 0;
+
+	 /*
 	 * while (i < 10)
 	 * {
 	 * putchar(i);
