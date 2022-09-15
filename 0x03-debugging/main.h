f0074a60e62fa6192316f3803f70e0819b3d1cda
walekazm <kazeem.nasiru@gmail.com>
2022-09-15 16:07:43 -0700

    all task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-
 int _putchar(char c);
-
-
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
