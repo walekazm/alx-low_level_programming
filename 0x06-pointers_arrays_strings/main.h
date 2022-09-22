6b1cf85b0d3847696285c7a7695010b63841f57d
walekazm <kazeem.nasiru@gmail.com>
2022-09-22 16:09:14 -0700

    All task

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c)
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
@@ -6,3 +8,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *)
 char *cap_string(char *);
 
+#endif
