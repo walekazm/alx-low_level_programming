b9a63bbee41a9a574d0366c82b36ecf55ac35fb1
walekazm <kazeem.nasiru@gmail.com>
2022-09-22 18:12:33 -0700

    All task

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ char *cap_string(char *);
 char *leet(char *s);
 char *rot13(char *);
 void print_number(int n);
+int main(void);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 #endif
